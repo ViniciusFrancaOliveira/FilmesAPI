4996bf0d0b9e3286ff94e729b6982746ddcdc6bc
vingadorv <vinifranca13@gmail.com>
2022-03-19 21:33:07 -0300

    Reset/Alteração de senha definição

--- UsuariosApi/Controllers/LoginController.cs
+++ UsuariosApi/Controllers/LoginController.cs
@@ -12,13 +12,30 @@ namespace UsuariosApi.Controllers
         private LoginService _loginService;
 
         public LoginController(LoginService loginService)
-        { 
+        {
             _loginService = loginService;
         }
 
         [HttpPost]
-        public IActionResult LogaUsuario(LoginRequest request) {
+        public IActionResult LogaUsuario(LoginRequest request)
+        {
             Result resultado = _loginService.LogaUsuario(request);
+            if (resultado.IsFailed) return Unauthorized(resultado.Reasons);
+            return Ok(resultado.Reasons);
+        }
+
+        [HttpPost("/solicita-reset")]
+        public IActionResult SolicitaResetSenhaUsuario(SolicitaResetRequest request)
+        {
+            Result resultado = _loginService.SolicitaResetSenhaUsuario(request);
+            if (resultado.IsFailed) return Unauthorized(resultado.Reasons);
+            return Ok(resultado.Reasons);
+        }
+
+        [HttpPost("/efetua-reset")]
+        public IActionResult ResetaSenhaUsuario(EfetuaResetRequest request)
+        {
+            Result resultado = _loginService.ResetaSenhaUsuario(request);
             if (resultado.IsFailed) return Unauthorized(resultado.Reasons);
             return Ok(resultado.Reasons);
         }
